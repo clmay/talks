bf587ffa05ed6c2e7666f76612f8a4feff66161a
Chase May <hello@clm.dev>
2024-01-05 12:48:32 -0700

    Fix build script

--- build-all.sh
+++ build-all.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
 output_dir=$1
+base_path=$2
 
 for dir in */; do
     if [ -f "$dir/package.json" ]; then
         if grep -q "slidev" "$dir/package.json"; then
             cd "$dir"
-            npx slidev build
-            cp -r ./dist/ ../$output_dir/$dir
+            npx slidev build --base /"$base_path"/"$dir"/
+            cp -r ./dist/ ../"$output_dir"/"$base_path"/"$dir"
             cd ..
         fi
     fi
